795b6609271a408d056bb1dc2acb4493b39bebab
Mohammed Yehia <m-ya@windowslive.com>
2018-05-23 13:07:08 +0300

    adding some modification Relates #15

--- public/js/fonteditor.js
+++ public/js/fonteditor.js
@@ -4,6 +4,12 @@ const fontFamilySelector = document.getElementById('font-family');
 const fontSize = document.getElementById('font-size');
 const fontSizeSpan = document.getElementById('font-size-value');
 
+const changeProperty = (propertyName, value) => {
+  [].forEach.call(navbarPreview.getElementsByTagName('li'), (element) => {
+    element.getElementsByTagName('a')[0].style[propertyName] = propertyName === 'fontSize' ? `${value}px` : value;
+  });
+};
+
 let selectedFontStyle = 'normal';
 let selectedFontWeight = 'normal';
 let selectedFontFamily = 'Arial';
@@ -11,14 +17,19 @@ let selectedFontFamily = 'Arial';
 
 fontStyleSelector.onchange = (e) => {
   selectedFontStyle = e.target.value;
+  changeProperty('fontStyle', selectedFontStyle);
 };
 
 fontWeightSelector.onchange = (e) => {
   selectedFontWeight = e.target.value;
+  changeProperty('fontWeight', selectedFontWeight);
 };
 fontFamilySelector.onchange = (e) => {
   selectedFontFamily = e.target.value;
+  changeProperty('fontFamily', selectedFontFamily);
 };
+
 fontSize.oninput = () => {
   fontSizeSpan.textContent = `${fontSize.value}px`;
+  changeProperty('fontSize', fontSize.value);
 };
