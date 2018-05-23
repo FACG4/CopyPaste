32ce8c9aebc9c20f1be0d10bf1b4e267c66e0c0d
sallamtanna <sallamtanna2015@hotmail.com>
2018-05-23 14:02:29 +0300

    adding style, adding generate code div

--- public/js/navbarDom.js
+++ public/js/navbarDom.js
@@ -13,89 +13,111 @@ const navbarPositionContainer = select('.navbarPositionContainer')
 const innerMainSectionForNavbar = select(".innerMainSectionForNavbar");
 const navbarPosition = select(".navbarPosition");
 const navbarPreview = select(".preview");
+const paddingInputForNavbar = select('.paddingInputForNavbar')
+const generateCodeButton = select('.generateCodeButton');
+const finalCssCode = select('.finalCssCode');
+const finalHtmlCode = select('.finalHtmlCode');
+const navEditContainer = select('.navEditContainer');
+const navPreview = select('.navPreview');
+const htmlCode = select('.htmlCode');
+const cssCode = select('.cssCode');
+const resultContainer = select('.resultContainer');
+const backToEditing = select('.backToEditing');
+const navHeight = select('#navHeight');
+
+
+
 let counter = 0;
+let flag = false;
+let newLinkTitle;
+let newLinkHref;
+let floatLabel;
+let floatInner;
+let defaultPositionForNavbar = "static";
+let defaultPAddingForNavbar = "x";
+let navbarHeight = '50';
+
+
 
 
 navbarPosition.addEventListener('change', () => {
-  if (this.checked) {
-    navbarPreview.style.navbarPosition = 'fixed';
+  if (!this.checked) {
+defaultPositionForNavbar = "fixed";
   }
 })
 
-const paddingLabel = create('span');
-paddingLabel.textContent = "Padding";
-paddingLabel.setAttribute("style", "margin-left:15px;margin-right:5px;");
-navbarPositionContainer.appendChild(paddingLabel);
-const paddingInputForNavbar = create('input');
-paddingInputForNavbar.setAttribute("type", "number");
-navbarPositionContainer.appendChild(paddingInputForNavbar);
 
 paddingInputForNavbar.addEventListener('input', () => {
-  navbarPreview.style.paddingInputForNavbar = paddingInputForNavbar.value + 'px';
+  navbarPreview.style.padding = paddingInputForNavbar.value + 'px';
+  defaultPAddingForNavbar=paddingInputForNavbar.value ;
+})
+
+navHeight.addEventListener('input', () => {
+  navbarPreview.style.height = navHeight.value + 'px';
+    navbarHeight=navHeight.value ;
 })
 
 addNewLinkBtn.addEventListener("click", () => {
-  counter++ ;
+
   const br = document.createElement("br");
-  const newLinkTitle = create('input');
-  newLinkTitle.setAttribute('id' , `newLinkTitle-${counter}`)
 
-  //adding event addEventListene
-  newLinkTitle.addEventListener('keyup', (e) => {
-    if (e.which == 13) {
-      newLiElementInNavbar = create('li');
-      newLiElementInNavbar.setAttribute('id' , `newLiElementInNavbar-${counter}`)
-      newHrefInNavbar = create('a');
-      navbarPreview.appendChild(newLiElementInNavbar);
-      newLiElementInNavbar.appendChild(newHrefInNavbar);
-      newHrefInNavbar.textContent = newLinkTitle.value;
-    }
-  })
 
-  newLinkTitle.setAttribute("class", "newLinkTitle");
-  newLinkTitle.setAttribute("placeholder", "Enter the text of the link...");
-  const href = create('span');
-  href.setAttribute("style", "margin-left:15px;margin-right:5px;");
-  newLinkTitle.setAttribute("style", "margin-left:30px;");
-  innerMainSectionForNavbar.appendChild(newLinkTitle);
-  const newLinkHref = create('input');
-  newLinkHref.setAttribute('id' , `newLinkHref-${counter}`)
+  if (flag === false) {
+    newLinkTitle = create('input');
+    newLinkHref = create('input');
+    floatInner = create('select');
+    floatLabel = create('span');
 
-  //adding event addEventListener
-  newLinkHref.addEventListener('keyup', (e) => {
-    if (e.which == 13) {
-      const newHref = newLinkHref.value;
-      newHrefInNavbar.setAttribute('href', newHref)
-    }
-  })
 
-  newLinkHref.setAttribute("class", "newLinkHref");
-  innerMainSectionForNavbar.appendChild(href);
-  href.textContent = "href";
-  innerMainSectionForNavbar.appendChild(newLinkHref);
-  newLinkHref.setAttribute("placeholder", "Enter the href...");
-  newLinkHref.setAttribute("style", "margin-right:15px;");
+    newLinkTitle.setAttribute('class', 'newLinkTitle');
+    newLinkTitle.setAttribute('id', `newLinkTitle-${counter}`)
+    newLinkTitle.setAttribute("class", "newLinkTitle");
+    newLinkTitle.setAttribute("placeholder", "Enter the text of the link...");
+    floatLabel.setAttribute("class", "label")
 
-  const newLinkFloat = create('span');
-  newLinkFloat.textContent = "Float";
-  innerMainSectionForNavbar.appendChild(newLinkFloat);
-  newLinkFloat.setAttribute("style", "margin-left:15px;margin-right:5px;");
-  const floatInner = create('select');
-  const option0 = create('option');
-  const option1 = create('option');
-  const option2 = create('option');
-  floatInner.id = "floatInner";
-  innerMainSectionForNavbar.appendChild(floatInner);
-  option0.text = 'select';
-  option1.value = `left-${counter}`;
-  option1.text = 'left';
-  option2.value = `right-${counter}`;
-  option2.text = 'right';
-  floatInner.appendChild(option0);
-  floatInner.appendChild(option1);
-  floatInner.appendChild(option2);
+    newLinkHref.setAttribute("placeholder", "Enter the href of the link...");
+    newLinkHref.setAttribute("class", "newLinkHref")
+    newLinkHref.setAttribute('id', `newLinkHref-${counter}`)
+    floatInner.setAttribute('class', 'floatInner');
 
-  floatInner.addEventListener('change', () => {
+    innerMainSectionForNavbar.appendChild(newLinkTitle);
+    innerMainSectionForNavbar.appendChild(newLinkHref);
+    innerMainSectionForNavbar.appendChild(floatLabel);
+    innerMainSectionForNavbar.appendChild(floatInner);
+
+    floatLabel.textContent = "Float";
+    const option0 = create('option');
+    const option1 = create('option');
+    const option2 = create('option');
+    floatInner.id = "floatInner";
+    option0.text = 'select';
+    option1.value = `left-${counter}`;
+    option1.text = 'left';
+    option2.value = `right-${counter}`;
+    option2.text = 'right';
+    floatInner.appendChild(option0);
+    floatInner.appendChild(option1);
+    floatInner.appendChild(option2);
+
+    flag = true;
+  } else if (flag === true) {
+
+    newLiElementInNavbar = create('li');
+    newLiElementInNavbar.setAttribute('id', `newLiElementInNavbar-${counter}`)
+    newHrefInNavbar = create('a');
+    navbarPreview.appendChild(newLiElementInNavbar);
+    newLiElementInNavbar.appendChild(newHrefInNavbar);
+    newHrefInNavbar.textContent = newLinkTitle.value;
+    // newHrefInNavbar.textContent = select(`#newLinkTitle-${counter}`).value;
+
+    const newHref = newLinkHref.value;
+    newHrefInNavbar.setAttribute('href', newHref)
+
+    newLinkHref.setAttribute("class", "newLinkHref");
+    floatLabel.setAttribute('class', 'floatLabel')
+
+
+
     let id = floatInner.value.split('-')[1];
     let float = floatInner.value.split('-')[0];
 
@@ -104,20 +126,66 @@ addNewLinkBtn.addEventListener("click", () => {
     } else {
       select(`#newLiElementInNavbar-${id}`).style.float = 'right';
     }
-  })
-  innerMainSectionForNavbar.appendChild(br);
+
+
+    innerMainSectionForNavbar.removeChild(newLinkTitle);
+    innerMainSectionForNavbar.removeChild(newLinkHref);
+    innerMainSectionForNavbar.removeChild(floatLabel);
+    innerMainSectionForNavbar.removeChild(floatInner);
+
+    flag = false;
+    counter++;
+  }
 });
 
 
 
-// size section
-const navWidth = document.getElementById('navWidth');
-const navHeight = document.getElementById('navHeight');
 
-navWidth.addEventListener('input', () => {
-  navbarPreview.style.width = navWidth.value + 'px';
+
+
+
+// generate code button >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+generateCodeButton.addEventListener('click', ()=>{
+
+
+
+  navEditContainer.style.display = 'none';
+  navPreview.style.display = 'none';
+  generateCodeButton.style.display = 'none';
+
+
+  resultContainer.style.display = 'block';
+  backToEditing.style.display = 'block';
+
+
+  finalCssCode.innerHTML = `ul{
+    padding: ${paddingInputForNavbar.value}px;<br>
+    position: ${defaultPositionForNavbar};<br>
+    height: ${navbarHeight}px;<br>
+    background-color:;<br>
+    width: 100%;} <br><br>
+    li a {<br>
+    padding: ${defaultPAddingForNavbar}
+    }
+    `
+
+finalHtmlCode.textContent = `<ul>
+  <li><a href="#home">Home</a></li>
+  <li><a href="#news">News</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li style="float:right"><a class="active" href="#about">About</a></li>
+</ul>`
 })
 
-navHeight.addEventListener('input', () => {
-  navbarPreview.style.height = navHeight.value + 'px';
+
+backToEditing.addEventListener('click' , ()=>{
+  navEditContainer.style.display = 'block';
+  navPreview.style.display = 'block';
+  generateCodeButton.style.display = 'block';
+
+
+  resultContainer.style.display = 'none';
+  backToEditing.style.display = 'none';
+
+
 })
